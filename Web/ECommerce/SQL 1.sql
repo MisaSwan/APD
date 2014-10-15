9f4c5da06fb77dd82638a2c2ddabbd02d4fcbfc4
João Arruda <joao.arruda@outlook.com>
2014-10-14 21:46:47 -0300

    asa

--- Web/ECommerce/SQL 1.sql
+++ Web/ECommerce/SQL 1.sql
@@ -9,31 +9,37 @@ CREATE TABLE Produto
     categoria VARCHAR(100)
 );
 
-INSERT INTO PRODUTO (imagem,nome,descricao,preco) VALUES(
+INSERT INTO PRODUTO (imagem,nome,descricao,preco,categoria) VALUES(
 ,'assets/img/galaxy.png'
 ,'Galaxy S5'
 ,'O melhor da Samsung no mercado'
-,1800);
+,1800
+,'Celular');
 INSERT INTO PRODUTO (imagem,nome,descricao,preco) VALUES('assets/img/iphone.png'
 ,'Iphone 4'
 ,'O mais estável da Apple no mercado'
-,600);
+,600
+,'Celular Bom');
 INSERT INTO PRODUTO (imagem,nome,descricao,preco) VALUES('assets/img/lumia.png'
 ,'Lumia 930'
 ,'O mais novo Windows Phone, com o melhor a te oferecer'
-,1200);
+,1200
+,'Smartphone');
 INSERT INTO PRODUTO (imagem,nome,descricao,preco) VALUES('assets/img/motox.png'
 ,'Moto X'
 ,'O melhor smartphone da Motorola'
-,900);
+,900
+,'Smartphone');
 INSERT INTO PRODUTO (imagem,nome,descricao,preco) VALUES('assets/img/xperia.png'
 ,'Xperia Z2'
 ,'O mais completo smartphone do mercado'
-2300);
+2300,
+'Smartphone');
 INSERT INTO PRODUTO (imagem,nome,descricao,preco) VALUES('assets/img/ArquivoExibir.jpeg'
 ,'Notebook 2 em 1 Touch'
 ,'Muito divertoso'
-,1400);
+,1400
+,'Notebook');
 
 INSERT INTO PRODUTO 
 (
